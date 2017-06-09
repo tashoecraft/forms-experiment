dc7df89b915e363224ff080da8e1ae83b8d0b11b
Austin Shoecraft <ashoecraft@jibe.com>
2017-06-09 19:07:38 -0400

    added comments for each value and function for abstract, control, group

--- src/app/ngrxForms/ngrx-forms.model.ts
+++ src/app/ngrxForms/ngrx-forms.model.ts
@@ -1,36 +1,65 @@
 import {NgrxFormActions} from './ngrx-forms.actions';
 
-export abstract class NgrxAbstractControl {
-  _value: any;
 
-  constructor() {}
+export abstract class NgrxAbstractControl {
+  // constructor(validator: ValidatorFn, asyncValidator: AsyncValidatorFn)
+  // validator : ValidatorFn How to do validations? possibly with effects, Do we need separate asyncValidators then?
+  // asyncValidator : AsyncValidatorFn
+  // get() value: Selector to value in store
+  // setValue: dispatch action to update value in store
+  // parent : FormGroup|FormArray
+  // status : string: VALID | INVALID | PENDING, selector to relevant place in store
+  // valid : boolean: selector to status, if VALID, return boolean
+  // invalid : boolean ^ opposite
+  // pending : boolean: Somehow detect if async validator hasn't finished, will need to keep track of observables that are running
+  // disabled : boolean :
+  // set() disabled : dispatch to store to update,
+  // enabled : boolean : default true
+  // errors : ValidationErrors| : collection of current errors, selector to store
+
+  // private _pristine: boolean = true:
+  // private _touched: boolean = false;
+  // private _parent: FormGroup|FormArray;: if there is a parent, then this will be the link between them
+
+  // markAsTouched () dispatch action to mark this control, and any ancestors as 'touched'
+  // markAsUnTouched() ^ opposite
+  // markAsDirty ^ just dirty
+  // markAsPristine ^
+  // markAsPending ^
+  // disable
+
+  // abstract
+  // setValue
+  // patchValue
+  // reset -> set to Original state
+
+
+  // get (name: string) returns control with provided name
+  // get root(): return root formGorup/array of form
 
 }
 
 export class NgrxFormControl extends NgrxAbstractControl {
-  public store: any;
-  constructor() {
-    super();
-  }
+  // What is a formControl besides a wrapper for abstract control without extras?
 }
 
 export class NgrxFormGroup extends NgrxAbstractControl {
-  public controls: {[key: string]: NgrxAbstractControl};
-  public store: any;
-  public parentPath;
-  private ngrxFormActions: NgrxFormActions;
+  // addControl (Control) -> adds control to list of controls, dispatches action to add control
+  // removeControl (Name) -> removes control from controls, dispathes actoin to remove control
 
-  constructor(name: string|number, store, parentPath: undefined[] = []) {
-    super();
-    this.ngrxFormActions = new NgrxFormActions();
+  // controls: selector to this formGroups controls
+  // registerControl() 'need?'
+  // addControl(name: string, control: AbstractControl) -> dispatch to add control to this formGroups controls
+  // removeControl(name: string) : void -> remove control from formGroup
+  // setControl(name: string, control: AbstractControl) -> replaces an existing control of same name, or just adds
+  // contains(controlName: string) : boolean -> whether control exists on group
+  // setValue(value: {[key: string]: any}, -> allows bulk update of controls values
+  // patchValue(value: {[key: string]: any}, {onlySelf, emitEvent}?: {onlySelf?: boolean, emitEvent?: boolean}) : void -> same except it doens't need all
+  // reset(value?: any, {onlySelf, emitEvent}?: {onlySelf?: boolean, emitEvent?: boolean}) : void -> goes through all descendants and resets them
+  // getRawValue() : any
 
-    store.dispatch(this.ngrxFormActions.createGroup(name));
-    this.store = store;
-    this.parentPath = parentPath;
-    this.parentPath.push(name);
-  }
+}
 
-  addControl(name: string, control?: NgrxAbstractControl): void {
-    this.store.dispatch(this.ngrxFormActions.addControl(name, this.parentPath));
-  }
+export class NgrxFormArray extends NgrxAbstractControl {
+  // mostly whats above, will come back
 }
